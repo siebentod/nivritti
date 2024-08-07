b70f780dbccc2172ff1946c06c9cc34c35cf9519
siebentod <33163165+siebentod@users.noreply.github.com>
2024-08-07 17:34:53 +0300

    small buttons improvements

--- src/App.jsx
+++ src/App.jsx
@@ -275,76 +275,36 @@ function App() {
     return (
       <div className="modeButtons">
         <button onClick={() => setMode('square')}>
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              <rect
-                x="4"
-                y="4"
-                width="16"
-                height="16"
-                rx="2"
-                stroke="#000000"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></rect>{' '}
-            </g>
+          <svg viewBox="0 0 24 24" fill="none">
+            <rect
+              x="4"
+              y="4"
+              width="16"
+              height="16"
+              rx="2"
+              stroke="#000000"
+              strokeWidth="2"
+            />
           </svg>
         </button>
         <button onClick={() => setMode('triangle')}>
-          <svg
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-          >
-            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              {' '}
-              <path
-                stroke="#000000"
-                strokeWidth="2"
-                d="M11.125 2.584a1 1 0 011.75 0l8.805 15.932A1 1 0 0120.805 20H3.195a1 1 0 01-.875-1.484l8.805-15.932z"
-              ></path>{' '}
-            </g>
+          <svg viewBox="0 0 24 24" fill="none">
+            <path
+              stroke="#000000"
+              strokeWidth="2"
+              d="M11.125 2.584a1 1 0 011.75 0l8.805 15.932A1 1 0 0120.805 20H3.195a1 1 0 01-.875-1.484l8.805-15.932z"
+            ></path>
           </svg>
         </button>
         <button onClick={() => setMode('circle')}>
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-            <g
-              id="SVGRepo_tracerCarrier"
+          <svg viewBox="0 0 24 24" fill="none">
+            <path
+              d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+              stroke="#000000"
+              strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
-            ></g>
-            <g id="SVGRepo_iconCarrier">
-              {' '}
-              <path
-                d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-                stroke="#000000"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></path>{' '}
-            </g>
+            ></path>
           </svg>
         </button>
       </div>
@@ -449,6 +409,7 @@ function App() {
           ) : (
             <>
               <button
+                autoFocus
                 className="chooseTime"
                 onClick={() => {
                   setCurrentTimer(120);
