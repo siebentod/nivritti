fc4ae6bea1898f01cfcb78e2d26e833ec29ac6dc
siebentod <33163165+siebentod@users.noreply.github.com>
2024-11-02 21:55:28 +0300

    smallfix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import vike from 'vike/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vike({
+      redirects: {
+        '/platon/symposion': '/plato-symposium',
+      },
+    }),
+  ],
 });
